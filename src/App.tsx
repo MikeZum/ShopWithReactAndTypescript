96fcc701d302e4ec3e860c552277dc258074a8f9
«Mike» <michail.v.tolmachev@gmail.com>
2022-07-29 18:24:24 +0800

    Добавил работу с сервером и loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Product } from './components/Product'
-import { products } from './data/products'
+// import { products } from './data/products'
+import axios from 'axios';
+import { IProduct } from './models'
 
 function App() {
+
+  const [products, setProduct] = useState<IProduct[]>([])
+  const [loading, setloading] = useState(false)
+
+  async function fetchProducts() {
+    setloading(true)
+    const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5')
+    setProduct(response.data);
+    setloading(false)
+  }
+
+  useEffect(() => {
+    fetchProducts()
+  }, [])
+
   return (
     <div className='container mx-auto max-w-2xl pt-5'>
-      <Product product={products[0]} />
-      <Product product={products[1]} />
+      {loading && <p className='text-center'>Loading...</p>}
+      {products.map(product => <Product product={product} key={product.id} />)}
     </div>
   );
 }
