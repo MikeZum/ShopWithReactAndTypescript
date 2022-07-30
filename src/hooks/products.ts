8f22f3db9ffe61a03b05895d8e4be5f66285b7bb
«Mike» <michail.v.tolmachev@gmail.com>
2022-07-30 16:07:29 +0800

    Доавил модальное окно

--- src/hooks/products.ts
+++ src/hooks/products.ts
@@ -3,16 +3,20 @@ import { IProduct } from '../models'
 import axios, { AxiosError } from 'axios'
 
 export function useProducts() {
-    const [products, setProduct] = useState<IProduct[]>([])
+    const [products, setProducts] = useState<IProduct[]>([])
     const [loading, setloading] = useState(false)
     const [error, setError] = useState('')
+
+    function addProduct(product: IProduct) {
+        setProducts(prev => [...prev, product])
+    }
 
     async function fetchProducts() {
         try {
             setError('')
             setloading(true)
             const response = await axios.get<IProduct[]>('https://fakestoreapi.com/products?limit=5')
-            setProduct(response.data);
+            setProducts(response.data);
             setloading(false)
         } catch (e: unknown) {
             const error = e as AxiosError
@@ -25,5 +29,5 @@ export function useProducts() {
         fetchProducts()
     }, [])
 
-    return { products, error, loading }
+    return { products, error, loading, addProduct }
 }
